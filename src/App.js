74389b3f14647ee2fe5023efd814aaca0778a38a
Toby Courtis <toby.courtis@hotmail.com>
2021-02-13 22:00:55 +0000

    Minor changes to show amin

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          How many people are shagging?
+          How many people are shagging now:
         </p>
         <CountUp
           start={start}
