19620d7f1b12cbedfda381fb72620d81fee48dbd
Amin Omarouayche <amin@planckti.me>
2021-02-13 23:55:06 +0000

    Changed date format

--- src/counter-logic/counter-logic.js
+++ src/counter-logic/counter-logic.js
@@ -1,9 +1,9 @@
 const max = 100 // 943356
 const min = 50//679542
 
-const maxTime = new Date('July 13, 1970 09:00:00 EST+00:00')
+const maxTime = new Date('Feb 13 1970 09:00:00 EST+0000')
 
-const time = new Date('July 13, 1970 15:00:00 EST+00:00')
+const time = new Date('Feb 13 1970 15:00:00 EST+0000')
 // use date now for production build
 // const time = new Date()
 
