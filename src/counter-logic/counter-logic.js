8f2fe7d9c582141cc58a7857109f3a40730bf876
Toby Courtis <toby.courtis@hotmail.com>
2021-02-14 16:04:45 +0000

    Fixed the logic for varying counter depending on how many humans are awake

--- src/counter-logic/counter-logic.js
+++ src/counter-logic/counter-logic.js
@@ -1,15 +1,18 @@
 const max = 100 // 943356
 const min = 50 //679542
 
+// 9am EST is the time where the most humans are awake
 const maxTime = new Date("Sat Feb 13 2021 09:00:00 GMT-0500")
-const time = new Date("Sat Feb 13 2021 15:00:00 GMT-0500")
-// use date now for production build
-// const time = new Date()
+let time = new Date()
 
 export function counterInputVars(){
-
-    var difference = Math.abs((time.getHours() - maxTime.getHours()))%12
-
+    time = new Date()
+    var difference = Math.abs(time.getUTCHours() - maxTime.getUTCHours())   
+    
+    if (difference > 12){
+        difference = 12 - difference%12
+    }
+    
     var numberOfPeople = max - difference*((max - min)/12)
 
     return {
