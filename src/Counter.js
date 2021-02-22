f7dd9697832a4780e25a7af7a7ca7d85633c135c
Amin Omarouayche <amin@planckti.me>
2021-02-22 20:28:04 +0000

    Refactored counter, changed name of logic function and removed update

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useCountUp } from 'react-countup';
-import {counterInputVars} from './counter-logic/counter-logic'
+import {currentNumberOfShaggers} from './counter-logic/counter-logic'
 
 function addNoise(maxFluctuation){
     // +1 required so it's between 1 and max value not 0 and max value - 1
@@ -8,30 +8,29 @@ function addNoise(maxFluctuation){
 }
 
 const Counter = () => {
-    
+
     const {
         countUp,
         update
     } = useCountUp({
-        end: counterInputVars(),
+        end: currentNumberOfShaggers(),
         duration: 3,
         separator: ",",
-        onUpdate: () => console.log('Updated!'),
         onEnd: () => noiseTimer()
     });
 
     function noiseTimer(){
         // update called twice below so there is a nice transition from initial count up and noise
-        update(counterInputVars() + addNoise(10))
+        update(currentNumberOfShaggers() + addNoise(10))
 
         const timer = setInterval(() => {
-            update(counterInputVars() + addNoise(10))
+            update(currentNumberOfShaggers() + addNoise(10))
         }, 1500);
 
         return () => {
             clearInterval(timer);
         };
-    }   
+    }
 
     return (
         <div>
