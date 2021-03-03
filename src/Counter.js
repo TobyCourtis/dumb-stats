5b67a820130f82c2b1da3ba99f2d79e1b8335e9c
Amin Omarouayache <79022616+am10n@users.noreply.github.com>
2021-03-03 21:33:03 +0000

    Added noise fluctuating according to a normal disctribution who's std dev. varies across the day. (#3)
    Co-authored-by: Amin Omarouayche <amin@planckti.me>

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useCountUp } from 'react-countup';
 import {currentNumberOfShaggers} from './counter-logic/counter-logic'
+import {addNoise} from './counter-logic/counter-logic'
 
-function addNoise(maxFluctuation){
-    // +1 required so it's between 1 and max value not 0 and max value - 1
-    return Math.floor(Math.random() * (2*maxFluctuation)) + 1 - maxFluctuation
-}
 
 const Counter = () => {
 
@@ -16,15 +13,15 @@ const Counter = () => {
         end: currentNumberOfShaggers(),
         duration: 3,
         separator: ",",
-        onEnd: () => noiseTimer()
+        onEnd: () => updateCounter()
     });
 
-    function noiseTimer(){
+    function updateCounter(){
         // update called twice below so there is a nice transition from initial count up and noise
-        update(currentNumberOfShaggers() + addNoise(10))
+        update(currentNumberOfShaggers() + addNoise())
 
         const timer = setInterval(() => {
-            update(currentNumberOfShaggers() + addNoise(10))
+            update(currentNumberOfShaggers() + addNoise())
         }, 1500);
 
         return () => {
--- src/counter-logic/counter-logic.js
+++ src/counter-logic/counter-logic.js
@@ -1,19 +1,35 @@
+import randomNormal from "random-normal";
+
 const max = 943356
 const min = 679542
 
+function findSecondsFromDate(date){
+    return (date.getUTCHours() * 3600) + (date.getUTCMinutes() * 60) + (date.getUTCSeconds())
+}
+
 // 9am EST is the time where the most humans are awake
-const maxTime = new Date("Sat Feb 13 2021 09:00:00 GMT-0500")
-let time = new Date()
+const maxDateTime = new Date("Sat Feb 13 2021 09:00:00 GMT-0500")
+const maxTimeInSeconds = findSecondsFromDate(maxDateTime)
+const secondsInTwelveHours = 43200
 
 export function currentNumberOfShaggers(){
-    time = new Date()
-    var difference = Math.abs(time.getUTCHours() - maxTime.getUTCHours())
+    let timeNow = new Date()
+    var difference = Math.abs(findSecondsFromDate(timeNow) - maxTimeInSeconds)
 
-    if (difference > 12){
-        difference = 12 - difference%12
+    if (difference > secondsInTwelveHours){
+        difference = secondsInTwelveHours - difference%secondsInTwelveHours
     }
 
-    var numberOfPeople = max - difference*((max - min)/12)
+    var numberOfPeople = max - difference*((max - min)/secondsInTwelveHours)
 
     return numberOfPeople
 }
+
+export function addNoise(maxFluctuation){
+    // +1 required so it's between 1 and max value not 0 and max value - 1
+
+    const deviation = (currentNumberOfShaggers()/max)*10
+
+    return randomNormal({mean: 0, dev: deviation})
+}
+
